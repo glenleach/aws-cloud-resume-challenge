93bc430c9cd26b24e2daa7b4afc9e5d7bb44fb28
glen.leach <glen.leach@yahoo.com>
2023-06-11 21:37:15 +0100

    terraform update

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -71,11 +71,11 @@ data "archive_file" "zip_the_python_code" {
 
 resource "aws_lambda_function_url" "url1" {
   function_name      = aws_lambda_function.myfunc.function_name
-  authorization_type = "NONE" 
+  authorization_type = "NONE"
 
   cors {
     allow_credentials = true
-    allow_origins     = ["https://www.glenleach.net", "https://resume.glenleasch.net"]
+    allow_origins     = ["*"]
     allow_methods     = ["*"]
     allow_headers     = ["date", "keep-alive"]
     expose_headers    = ["keep-alive", "date"]
