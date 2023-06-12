7a75e1d1bbdeec36cc33563095913cd0fee36aa9
glen.leach <glen.leach@yahoo.com>
2023-06-12 05:46:30 +0100

    update js/index.html

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://6eyootm37nsiyt4jpzaiebcewy0ufcsv.lambda-url.eu-west-2.on.aws/");
+    let response = await fetch("https://zcw3ccloladwt4cpqb2sfypdu40xpiuy.lambda-url.eu-west-2.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` views: ${data}`;
 }
