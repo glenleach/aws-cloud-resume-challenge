10727a2424045d17d51c1413e3d05e9d716c479c
glen.leach <glen.leach@yahoo.com>
2023-06-14 22:33:31 +0100

    update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,14 +1,15 @@
-resource "aws_lambda_function" "myfunc1" {
+resource "aws_lambda_function" "myfunc" {
   filename         = data.archive_file.zip_the_python_code.output_path
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
   function_name    = "myfunc"
-  role             = aws_iam_role.iam_for_lambda1.arn
-  handler          = "func.handler"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "func.lambda_handler"
   runtime          = "python3.8"
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
-resource "aws_iam_role" "iam_for_lambda1" {
-  name = "iam_for_lambda1"
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
   assume_role_policy = <<EOF
 {
@@ -27,6 +28,43 @@ resource "aws_iam_role" "iam_for_lambda1" {
 EOF
 }
 
+resource "aws_iam_policy" "iam_policy_for_resume_project" {
+
+  name        = "aws_iam_policy_for_terraform_resume_project_policy"
+  path        = "/"
+  description = "AWS IAM Policy for managing the resume project role"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource" : "arn:aws:logs:*:*:*",
+          "Effect" : "Allow"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "dynamodb:UpdateItem",
+            "dynamodb:GetItem",
+            "dynamodb:PutItem"
+          ],
+          "Resource" : "arn:aws:dynamodb:*:*:table/cloud-resume"
+        },
+      ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.iam_policy_for_resume_project.arn
+
+}
+
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
   source_file = "${path.module}/lambda/func.py"
@@ -34,7 +72,7 @@ data "archive_file" "zip_the_python_code" {
 }
 
 resource "aws_lambda_function_url" "url1" {
-  function_name      = aws_lambda_function.myfunc1.function_name
+  function_name      = aws_lambda_function.myfunc.function_name
   authorization_type = "NONE"
 
   cors {
